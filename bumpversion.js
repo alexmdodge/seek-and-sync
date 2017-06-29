d485087d1909f9bb9aa49836b3713bc394df7e39
Alex Dodge <alex.dodge@redspace.com>
2017-06-29 14:44:07 -0300

    sns: add bumps back

--- bumpversion.js
+++ bumpversion.js
@@ -19,39 +19,39 @@ if ( shell.exec(command).code === 0) {
   process.exit(-1);
 }
 
-// // Update the file list with all desired version files
-// var fileList = [
-//   './index.js',
-// ]
+// Update the file list with all desired version files
+var fileList = [
+  './index.js',
+]
 
-// // There could be an options object for reach file depending
-// // on version specificity
-// var options = {
-//   type: process.argv[2],
-//   key: 'version',
-// }
+// There could be an options object for reach file depending
+// on version specificity
+var options = {
+  type: process.argv[2],
+  key: 'version',
+}
 
-// // Loop through each file in the list, pass it through the bump
-// // function, then return the result
-// fileList.forEach(file => fs.readFile(file, 'utf8', (err, data) => {
-//   if ( err ) {
-//     console.log(`Error: ${err}`);
-//   } else {
-//     options.str = data;
+// Loop through each file in the list, pass it through the bump
+// function, then return the result
+fileList.forEach(file => fs.readFile(file, 'utf8', (err, data) => {
+  if ( err ) {
+    console.log(`Error: ${err}`);
+  } else {
+    options.str = data;
 
-//     // Bump version of all the strings
-//     bump(options, (err, out) => {
-//       if ( err) {
-//         console.log(`Error: ${err}`);
-//       } else {
-//         fs.writeFile(file, out.str, err => {
-//           if (err) {
-//             console.log(`Error: ${err}`);
-//           } else {
-//             console.log(`File bumped: ${file}`);
-//           }
-//         });
-//       }
-//     });
-//   }
-// }));
+    // Bump version of all the strings
+    bump(options, (err, out) => {
+      if ( err) {
+        console.log(`Error: ${err}`);
+      } else {
+        fs.writeFile(file, out.str, err => {
+          if (err) {
+            console.log(`Error: ${err}`);
+          } else {
+            console.log(`File bumped: ${file}`);
+          }
+        });
+      }
+    });
+  }
+}));
