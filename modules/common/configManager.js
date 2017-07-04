b3ccbf99d2ba6e965745d74251c21b81faf4d093
Alex Dodge <alex.dodge@redspace.com>
2017-07-04 11:54:40 -0300

    sns: config manager to handle prompts

---
+++ modules/common/configManager.js
@@ -0,0 +1,88 @@
+const chalk = require('chalk');
+const common = require('common-tags');
+const utils = require('../common/utils.js');
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+/**
+ * Utility class for managing the configuration object
+ * during the 
+ * 
+ * @class Config
+ * @param conf the configuration to test
+ */
+class ConfigManager {
+
+  static verifyConfiguration(conf) {
+    console.log('\n',common.stripIndents`
+      ${chalk.bold('Your current configuration is')} :
+    `);
+
+    let isUnfilled = false;
+    let value;
+    Object.keys(conf).map(key => {
+      if ( conf[key] === null ) {
+        isUnfilled = true;
+        value = `${chalk.yellow('Value Undefined')}`;
+      } else {
+        value = conf[key];
+      }
+      console.log(`  • ${chalk.white(key)}: ${value}`);
+    });
+
+    // If unfilled value, then exit with error
+    if ( isUnfilled ) {
+      console.log('\n');
+      throw new Error(common.stripIndents`
+        You have not specified a parent path. Either pass the parent path to sync through the -p flag, or specify a 
+        static path in the ${chalk.white('.snsrc')} configuration file.
+      `);
+    } else {
+      console.log('');
+      console.log('Proceed with sync based on the above configuration? (yes/no)');
+      reader.prompt('> ');
+      reader.on('line', input => {
+        if (utils.matchValueIgnoreCase(input, ['yes', 'y'])) {
+          console.log('--- Starting Sync ---');
+          process.exit(1);
+        } else {
+          console.log(`${chalk.yellow('Exiting sync')}. Please update configuration with desired settings.`);
+          process.exit(1);
+        }
+      });
+    }
+  }
+
+  static initializeConfiguration(conf) {
+    // Check that configuration setup properly
+    if ( !conf ) {
+      console.log('\n',common.stripIndents`
+        ${chalk.red('Configuration Error')}: Unable to load configuration. Ensure
+        ${chalk.white('.snsrc')} file is present in parent directory.
+      `);
+      throw new Error('Configuration error.');
+    } else if ( conf.name === null ) {
+      console.log('\n',common.stripIndents`
+        ${chalk.red('Configuration Error')}: Please ensure a project name is defined
+        in the ${chalk.white('.snsrc')} configuration file.
+      `);
+      throw new Error('Configuration error.');      
+    } else if ( conf.path === null ) {
+      console.log('\n',common.stripIndents`
+        ${chalk.red('Configuration Error')}: Please ensure a project path is defined
+        in the ${chalk.white('.snsrc')} configuration file.
+      `);
+      throw new Error('Configuration error.');      
+    } else if ( conf.parent_path === null ) {
+      console.log('\n',common.stripIndents`
+        ${chalk.yellow('Configuration Warning')}: You have not defined a parent path for your project.
+        Please ensure when calling the sync tool to provide a path where the parent project is located.
+      `);
+    }
+  }
+}
+
+module.exports = ConfigManager;
---
+++ modules/common/configManager.test.js
@@ -0,0 +1,36 @@
+const configManager = require('./configManager.js');
+const defaultConf = require('./defaultConf.json');
+
+/**
+ * Wraps a set timeout in a promise to delay the check
+ * for a value related to async functions.
+ */
+async function delayCheck(value, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout((value, time) => {
+      resolve(value);
+    }, time);
+  });
+}
+
+/**
+ * Function: verify
+ * Checks the provided user configuration and prompts
+ * them to keep or make modifications if errors.
+ */
+describe('verify()', () => {
+  test('should be called', () => {
+    expect(configManager.verifyConfiguration(defaultConf));
+  });
+});
+
+/**
+ * Function: initializeConfiguration
+ * A series of steps to check initial user configuration and
+ * provide warnings and support for invalid setup.
+ */
+describe('initializeConfiguration()', () => {
+  test('should be called', () => {
+    expect(configManager.initializeConfiguration(defaultConf));
+  });
+})
